2a325bd6c5470649991a4e3546ac964b64d2ef3e
Lucas Lima <lucas.c.lima70@gmail.com>
2025-12-22 15:11:21 -0500

    Update to app.py to test ci/cd workflows

--- app/hello-python/app.py
+++ app/hello-python/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World from Flask running on EKS via Helm & Jenkins!"
+    return "Hello World from Flask running on EKS via Helm & Jenkins!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
