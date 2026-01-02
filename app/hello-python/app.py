295f51d0b00e7a56f9b26377dc8ebda75913d185
Lucas Lima <lucas.c.lima70@gmail.com>
2026-01-01 20:47:28 -0500

    Argo CD Prod deploy

--- app/hello-python/app.py
+++ app/hello-python/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World from Flask running on EKS via Helm & Jenkins!! ARGOCD BUILD"
+    return "Hello World from Flask running on EKS via Helm. PROD via ArgoCD!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
