eef868110041d33cfc0bb48783e65d95b0b552f1
Lucas Lima <lucas.c.lima70@gmail.com>
2026-01-03 18:56:43 -0500

    DEV: fix node-exporter capacity (t3.medium) + node_group_name_prefix + restore ECR lifecycle policy

--- terraform/modules/node-group/main.tf
+++ terraform/modules/node-group/main.tf
@@ -2,10 +2,10 @@
 # EKS Managed Node Group
 # -------------------------
 resource "aws_eks_node_group" "this" {
-  cluster_name    = var.cluster_name
-  node_group_name = var.node_group_name
-  node_role_arn   = var.node_role_arn
-  subnet_ids      = var.subnet_ids
+  cluster_name           = var.cluster_name
+  node_group_name_prefix = var.node_group_name_prefix
+  node_role_arn          = var.node_role_arn
+  subnet_ids             = var.subnet_ids
 
   scaling_config {
     desired_size = var.desired_size
